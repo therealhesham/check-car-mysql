19e48351e8b3838de4da111f5df9ad51866ef726
Your Name <you@example.com>
2025-06-02 16:37:21 +0300

    حل مشكلة مسح التوقيع

--- app/cheak-out/page.tsx
+++ app/cheak-out/page.tsx
@@ -1348,7 +1348,7 @@ getCar(selectedPlate)
                       type="button"
                       onClick={handleClearSignature}
                       className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
-                      disabled={!signatureUrl}
+                      
                     >
                       مسح التوقيع
                     </button>
